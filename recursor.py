7714f8edb526e3d4685e003b48b397b64be37de2
minervathlmn <mlim6947@uni.sydney.edu.au>
2023-10-04 23:31:28 +1100

    first commit

---
+++ recursor.py
@@ -0,0 +1,15 @@
+"""
+Write code for your recursor here.
+
+You may import library modules allowed by the specs, as well as your own other modules.
+"""
+from sys import argv
+
+
+def main(args: list[str]) -> None:
+    # TODO
+    pass
+
+
+if __name__ == "__main__":
+    main(argv[1:])
