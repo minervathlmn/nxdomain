020067feb80c8e95d52a8e2c607247e00be2c7b1
Kaizen (改善) <99392055+sixthmelb@users.noreply.github.com>
2023-10-15 17:26:27 +0800

    Update recursor.py

--- recursor.py
+++ recursor.py
@@ -1,115 +1,88 @@
 import sys
 import socket
+import time
 
 # Check the number of command-line arguments
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     print("INVALID ARGUMENTS")
     sys.exit(1)
 
-# Extract the configuration file path from the command-line argument
-config_file = sys.argv[1]
+# Extract the command-line arguments
+root_port = int(sys.argv[1])
+timeout = float(sys.argv[2])
 
-# Initialize a dictionary to store hostname to port mappings
-hostname_to_port = {}
-
-# Load the configuration from the file
-try:
-    with open(config_file, "r") as file:
-        lines = file.readlines()
-        # The first line contains the port number the server occupies
-        server_port = int(lines[0].strip())
-        # Read the rest of the lines to populate the hostname_to_port dictionary
-        for line in lines[1:]:
-            parts = line.strip().split(',')
-            if len(parts) == 2:
-                hostname, port = parts
-                hostname_to_port[hostname] = int(port)
-except (FileNotFoundError, PermissionError, ValueError):
-    print("INVALID CONFIGURATION")
+# Validate the root port range
+if not (1024 <= root_port <= 49151):
+    print("INVALID PORT NUMBER")
     sys.exit(1)
 
-# Create a buffer to store incomplete messages
-buffer = ""
+# Function to query a DNS server and handle timeouts
+def query_dns(server, query, port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((server, port))
+            s.send(query.encode())
+            response = s.recv(1024).decode()
+            return response
+    except (ConnectionRefusedError, TimeoutError):
+        return None
 
-# Function to handle the ADD command
-def handle_add_command(command, connection):
-    # Extract HOSTNAME and PORT from the command
-    parts = command.split()
-    if len(parts) == 3:
-        _, hostname, port = parts
-        if hostname.isalnum() and port.isdigit():
-            port = int(port)
-            hostname_to_port[hostname] = port
-            connection.send(f"OK\n".encode())
-        else:
-            connection.send("INVALID\n".encode())
-    else:
-        connection.send("INVALID\n".encode())
+# Function to resolve a domain
+def resolve_domain(domain):
+    start_time = time.time()
 
-# Function to handle the DEL command
-def handle_del_command(command, connection):
-    # Extract HOSTNAME from the command
-    parts = command.split()
-    if len(parts) == 2:
-        _, hostname = parts
-        if hostname in hostname_to_port:
-            del hostname_to_port[hostname]
-            connection.send(f"OK\n".encode())
-        else:
-            connection.send("INVALID\n".encode())
-    else:
-        connection.send("INVALID\n".encode())
+    # Step 1: Query the root server
+    root_response = query_dns("localhost", domain + "\n", root_port)
+    if root_response is None:
+        print("FAILED TO CONNECT TO ROOT")
+        sys.exit(1)
 
-# Function to handle the EXIT command
-def handle_exit_command(connection):
-    connection.send(f"Goodbye\n".encode())
-    connection.close()
-    sys.exit(0)
-
-# Function to resolve a hostname to a port and log the result
-def resolve_hostname(hostname, connection):
-    if hostname in hostname_to_port:
-        port = hostname_to_port[hostname]
-        connection.send(f"{port}\n".encode())
-        print(f"resolve {hostname} to {port}")
-    else:
-        connection.send("resolve NXDOMAIN\n".encode())
-        print(f"resolve {hostname} to NXDOMAIN")
-
-# Main server loop
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind(("localhost", server_port))
-    server_socket.listen()
-
-    print(f"Server is listening on port {server_port}")
-
-    while True:
+    # Step 2: Extract TLD server port from the root response
+    root_response_parts = root_response.strip().split()
+    if len(root_response_parts) == 4 and root_response_parts[0] == "resolve":
         try:
-            connection, address = server_socket.accept()
-            data = connection.recv(1024).decode()
-            if not data:
-                break
+            tld_port = int(root_response_parts[3])
+        except ValueError:
+            print("Invalid port in root response")
+            sys.exit(1)
+    else:
+        print(f"{root_response}")
+        sys.exit(1)
 
-            # Handle incomplete messages and accumulate them in the buffer
-            buffer += data
-            if '\n' not in buffer:
-                continue
+    # Step 3: Query the TLD server
+    tld_response = query_dns("localhost", ".".join(domain.split('.')[1:]) + "\n", tld_port)
+    if tld_response is None:
+        print("NXDOMAIN")
+        sys.exit(1)
 
-            # Split the complete messages in the buffer
-            messages = buffer.split('\n')
-            # The last element in messages may be an incomplete message
-            buffer = messages[-1]
-            messages = messages[:-1]
+    # Step 4: Extract authoritative server port from the TLD response
+    try:
+        auth_port = int(tld_response.strip())
+    except ValueError:
+        print("INVALID TLD RESPONSE")
+        sys.exit(1)
 
-            # Process complete messages
-            for message in messages:
-                if message.startswith('!ADD'):
-                    handle_add_command(message, connection)
-                elif message.startswith('!DEL'):
-                    handle_del_command(message, connection)
-                elif message == '!EXIT':
-                    handle_exit_command(connection)
-                else:
-                    resolve_hostname(message, connection)
-        except KeyboardInterrupt:
-            sys.exit(0)
+    # Step 5: Query the authoritative server
+    auth_response = query_dns("localhost", domain + "\n", auth_port)
+    if auth_response is None:
+        print("NXDOMAIN")
+        sys.exit(1)
+
+    # Step 6: Print the resolved port
+    print(auth_response.strip())
+
+    elapsed_time = time.time() - start_time
+
+    if elapsed_time > timeout:
+        print("TIMEOUT")
+        sys.exit(1)
+
+# Main program
+while True:
+    try:
+        user_input = input("Enter a domain (Ctrl-D to exit): ")
+        if not user_input:
+            break
+        resolve_domain(user_input)
+    except KeyboardInterrupt:
+        break
