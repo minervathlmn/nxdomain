ba7ade9ff72d203fac19c152f1a0badd3924da76
Kaizen (改善) <yogijr510@gmail.com>
2023-10-16 15:14:22 +0800

    Add simple unittest method

--- tests/run.sh
+++ tests/run.sh
@@ -9,5 +9,5 @@
 # Run the tests
 for test_file in tests/*.in; do
     base_name=$(basename -s .in $test_file)
-    python recursor.py 1024 5 < "$test_file" | diff - "tests/${base_name}.out" > "tests/${base_name}.diff"
+    python3 recursor.py 1024 5 < "$test_file" | diff - "tests/${base_name}.out" > "tests/${base_name}.diff"
 done
